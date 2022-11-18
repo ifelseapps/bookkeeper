c7200eabfbe54074a8f941630232b2db9a786545
Maxim Pavlov <ifelseapps@gmail.com>
2022-11-18 07:40:19 +0300

    Настройка БД

---
+++ db/init/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  id varchar(40) primary key,
+  name varchar(255) NOT NULL,
+  middle_name varchar(255) NOT NULL,
+  surname varchar(255) NOT NULL,
+  login varchar(255) NOT NULL,
+  password varchar(255) NOT NULL
+);
