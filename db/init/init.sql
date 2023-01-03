eea0b99bbd795f07205fbf85f4ccffd08d2bd917
Maxim Pavlov <ifelseapps@gmail.com>
2023-01-03 21:12:57 +0300

    (Backend) Инициализация Laravel

--- db/init/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id varchar(40) primary key,
-  name varchar(255) NOT NULL,
-  middle_name varchar(255) NOT NULL,
-  surname varchar(255) NOT NULL,
-  login varchar(255) NOT NULL,
-  password varchar(255) NOT NULL
-);
